b22cb282daf8793dbc6fa6cffaa75071e3f834f5
Robert-Martins <robertcantares@gmail.com>
2023-04-03 22:17:42 -0300

    refactor/changing-map-strategy

--- src/models/task.model.ts
+++ src/models/task.model.ts
@@ -1,7 +1,24 @@
-export interface Task {
+export class Task {
+    id: number;
     title: string;
     text: string;
     done: boolean;
     toBeDone: Date;
     createdAt: Date;
+
+    constructor(
+        id: number,
+        title: string,
+        text: string,
+        done: boolean,
+        toBeDone: Date,
+        createdAt: Date,
+    ){
+        this.id = id;
+        this.title = title;
+        this.text = text;
+        this.done = done;
+        this.toBeDone = toBeDone;
+        this.createdAt = createdAt;
+    }
 }
--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,6 +1,18 @@
-export interface User {
+export class User {
     name: string;
     totalTasks: number;
     tasksConcluded: number;
     createdAt: Date;
+
+    constructor(
+        name: string,
+        totalTasks: number,
+        tasksConcluded: number,
+        createdAt: Date
+    ) {
+        this.name = name;
+        this.totalTasks = totalTasks;
+        this.tasksConcluded = tasksConcluded;
+        this.createdAt = createdAt;
+    }
 }
