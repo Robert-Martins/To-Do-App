4e18fa03feca3d235b054be31bd9b6120abe8ec2
Robert-Martins <robertcantares@gmail.com>
2023-04-17 19:36:18 -0300

    feat/counter-enhancement

--- src/components/atoms/counters/DateCounter.tsx
+++ src/components/atoms/counters/DateCounter.tsx
@@ -7,8 +7,8 @@ export const DateCounter = (props: Props) => {
     const {date} = props;
 
     return (
-        <div className="center-text">
-            <h6>
+        <div className="center-text p-8-16 counter border-4">
+            <h6 className="m-bottom-8">
                 User since
             </h6>
             <p>
--- src/components/atoms/counters/TasksCounter.tsx
+++ src/components/atoms/counters/TasksCounter.tsx
@@ -8,8 +8,8 @@ export const TasksCounter = (props: Props) => {
     const {label, amount} = props;
 
     return (
-        <div className="center-text">
-            <h6>
+        <div className="center-text p-8-16 counter border-4">
+            <h6 className="m-bottom-8">
                 {label}
             </h6>
             <p>
